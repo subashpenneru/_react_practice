71d2222c54c516bcafb27897abdb50f5f0f09eb9
subashpenneru <subashpenneru@gmail.com>
2020-05-16 15:28:36 +0530

    Debugging

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,12 @@ import classes from './Person.css';
 
 const person = (props) => {
     let child = props.children ? <p> { props.children } </p> : null;
+
+    let rndm = Math.random();
+    if(rndm > 0.6) {
+        throw new Error(`${rndm}`);
+    }
+
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm { props.name } and I am { props.age } years old!</p>
