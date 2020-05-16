a24391706650780b9777644975cfd5b3d8b0edca
subashpenneru <subashpenneru@gmail.com>
2020-05-16 11:32:41 +0530

    CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -36,16 +36,10 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
 
     let persons = null;
+    let btnClass = '';
+
     if(this.state.showPersons) {
       persons = (
         <div>
@@ -60,28 +54,26 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I am a React Developer</h1>
-        <p className={classes.join(' ')}>Sai Subash Penneru</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <p className={assignedClasses.join(' ')}>Sai Subash Penneru</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
   }
-
-  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null,  `Hi, I'm a React Developer`));
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
     let child = props.children ? <p> { props.children } </p> : null;
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm { props.name } and I am { props.age } years old!</p>
             { child }
             <input type="text" onChange={ props.changed } value={ props.name } />
