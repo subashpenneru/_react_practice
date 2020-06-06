532897fdac551728c8259282c15988e1e524498f
subashpenneru <subashpenneru@gmail.com>
2020-06-06 17:40:40 +0530

    React Animations

--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import counterReducer from './store/reducers/counter';
-import resultReducer from './store/reducers/result';
 
-const rootReducer = combineReducers({
-    counter: counterReducer,
-    result: resultReducer
-});
-
-const logger = store => {
-    return next => {
-        return action => {
-            const result = next(action);
-            return result;
-        }
-    }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)))
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/store/utility.js
+++
@@ -1,3 +0,0 @@
-export const updateObject = (oldObject, updatedValues) => {
-    return { ...oldObject, ...updatedValues }
-};
